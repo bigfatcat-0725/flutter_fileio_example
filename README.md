ed3134332da03f5df179879dcd0109f2d9710907
bigfatcat <bigfatcat0725@gmail.com>
2022-03-19 21:37:27 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_fileio_example
+
+path_provider
---
+++ android/app/src/main/kotlin/com/bigfatcat0725/flutter_fileio_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bigfatcat0725.flutter_fileio_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
